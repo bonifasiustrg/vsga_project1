ed5758aa758e91d7957acfa546ceeb7712ea3920
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2022-08-02 00:24:04 +0700

    Create option menu and optimize the function

--- app/src/main/java/com/takasima/vsga_project1/MainActivity.java
+++ app/src/main/java/com/takasima/vsga_project1/MainActivity.java
@@ -1,8 +1,13 @@
 package com.takasima.vsga_project1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Profil");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater pengisiMenu = getMenuInflater();
+        pengisiMenu.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intentPindah;
+        if (item.getItemId() == R.id.m_name) {
+                intentPindah = new Intent(this, NameActivity.class);
+        }else if (item.getItemId() == R.id.m_calculator) {
+                intentPindah = new Intent(this, CalculatorActivity.class);
+        }else {
+                intentPindah = new Intent(this, ListOfCountry.class);
+        }
+
+        startActivity(intentPindah);
+        return true;
     }
 }
